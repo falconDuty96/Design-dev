a30a18143c4c5f34e571d1a9b8169900c02ca8bd
falconDuty96 <falcon.duty96@gmail.com>
2023-02-09 11:19:42 +0300

    Test Integration Github hosting

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,12 @@ $(document).ready(function() {
     $(".icon-menu").each(function() {
         $(this).on({
             mouseenter: function() {
-                console.log($(this))
+                // var icon = $(this).children()[0].attr("scr") ;
+                console.log($(this).children()[0])
+                // console.log($(this).children()[0].attr("scr"))
+                // var dataIcon = icon.split('.') ;
+                // var newIcon = dataIcon[0]+"-colored"+".png" ;
+                // $(this).children()[0].attr("scr","../icone/"+newIcon) ;
             },
             mouseleave: function() {
                 
