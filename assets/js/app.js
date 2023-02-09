2a2598f8786e8049b3c03359b678846dce92beb2
falconDuty96 <falcon.duty96@gmail.com>
2023-02-09 11:39:09 +0300

    Icon + Text modification

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,25 @@
 $(document).ready(function() {
-
+    var stateIcon ;
+    var stateTxt ;
     $(".icon-menu").each(function() {
         $(this).on({
+            
             mouseenter: function() {
-                // var icon = $(this).children()[0].attr("scr") ;
-                console.log($(this).children()[0])
-                // console.log($(this).children()[0].attr("scr"))
-                // var dataIcon = icon.split('.') ;
-                // var newIcon = dataIcon[0]+"-colored"+".png" ;
-                // $(this).children()[0].attr("scr","../icone/"+newIcon) ;
+                var icon = $(this).children()[0].attributes[0].nodeValue ;
+                stateIcon = icon ;
+                var dataIcon = icon.split('.') ;
+                var newIcon = dataIcon[0]+"-colored"+".png" ;
+                $(this).children()[0].attributes[0].nodeValue = newIcon ;
+                stateTxt = $(this).children()[1].attributes["class"].nodeValue ;
+                $(this).children()[1].attributes["class"].nodeValue += " text-and-icon" ;
             },
             mouseleave: function() {
-                
+                $(this).children()[0].attributes[0].nodeValue = stateIcon ;
+                $(this).children()[1].attributes["class"].nodeValue = stateTxt ;
+                stateIcon = "" ;
+                stateTxt = "" ;
             }
         })
     })
+
 })
