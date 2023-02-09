a9c23d32d70870c81a1cbd56da4d3a8938ba4e95
falconDuty96 <falcon.duty96@gmail.com>
2023-02-09 15:15:03 +0300

    Adding functionnality: Hover menu. There is issue but update on next commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,27 @@
 $(document).ready(function() {
     var stateIcon ;
     var stateTxt ;
+    var elmentToChange = [] ;
+    var initialConfig = 0 ;
+    var anim ;
     $(".icon-menu").each(function() {
         $(this).on({
             
             mouseenter: function() {
+                console.log("Miditra icone")
+                clearTimeout(anim)
+                
+                
+                
+
+                var d = $(this).attr("data-shower") ;
+                $('.boite-menu').css({
+                    "display":"none"
+                })
+                $("#boite-"+d).css({
+                    "display":"flex" ,
+                })
+                elmentToChange = $(this).children() ;
                 var icon = $(this).children()[0].attributes[0].nodeValue ;
                 stateIcon = icon ;
                 var dataIcon = icon.split('.') ;
@@ -12,12 +29,49 @@ $(document).ready(function() {
                 $(this).children()[0].attributes[0].nodeValue = newIcon ;
                 stateTxt = $(this).children()[1].attributes["class"].nodeValue ;
                 $(this).children()[1].attributes["class"].nodeValue += " text-and-icon" ;
+               
             },
-            mouseleave: function() {
-                $(this).children()[0].attributes[0].nodeValue = stateIcon ;
-                $(this).children()[1].attributes["class"].nodeValue = stateTxt ;
-                stateIcon = "" ;
-                stateTxt = "" ;
+            mouseleave: function(a) {
+                var d = $(this).attr("data-shower") ;
+                
+                anim = setTimeout(function() {
+                    
+                    $("#boite-"+d).css({
+                        "display":"none" ,
+                    })
+                    console.log("Nandeha ilay timer")
+                    elmentToChange[0].attributes[0].nodeValue = stateIcon ;
+                    elmentToChange[1].attributes["class"].nodeValue = stateTxt ;
+                    stateIcon = "" ;
+                    stateTxt = "" ;
+                    elmentToChange = [] ;
+                },5) ;
+                $("#boite-"+d).on({
+                    mouseenter: function(c) {
+                        c.stopImmediatePropagation()
+                        console.log("Miditra boite")
+                        clearTimeout(anim) ;
+                    },
+                    mouseleave: function(d) {
+                        d.stopImmediatePropagation()
+                        console.log("Mivoaka boite")
+                        clearTimeout(anim) ;
+                        $(this).css({
+                            "display":"none" ,
+                        })
+                        console.log(elmentToChange)
+                        elmentToChange[0].attributes[0].nodeValue = stateIcon ;
+                        elmentToChange[1].attributes["class"].nodeValue = stateTxt ;
+                        stateIcon = "" ;
+                        stateTxt = "" ;
+                        elmentToChange = [] ;
+                        
+                    }
+                    
+                })
+
+
+                
             }
         })
     })
